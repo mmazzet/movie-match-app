a3e52a912400d3348f75fc7265bc5813fb3b3428
Marianna Mazzette <20104120@mail.wit.ie>
2026-03-23 17:00:25 +0000

    update db url

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,7 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "postgresql+psycopg2://postgres:postgres@localhost:5432/movie_match"
+load_dotenv()
+
+DATABASE_URL = (
+    f"postgresql+psycopg2://{os.getenv('POSTGRES_USER')}:"
+    f"{os.getenv('POSTGRES_PASSWORD')}@"
+    f"{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/"
+    f"{os.getenv('POSTGRES_DB')}"
+)
 
 engine = create_engine(DATABASE_URL)
 
