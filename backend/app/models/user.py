161fd87e4f65e01b70da09d2c3c84e216b057292
Marianna Mazzette <20104120@mail.wit.ie>
2026-03-24 15:47:59 +0000

    update users table

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -8,5 +8,5 @@ class User(Base):
   __tablename__ = "users" 
   id = Column(Integer, primary_key=True, index=True)
   email = Column(String, unique=True, index=True, nullable=False)
-  password_hash = Column(String, nullable=False)
+  password = Column(String, nullable=False)
   created_at = Column(DateTime(timezone=True), server_default=func.now())
