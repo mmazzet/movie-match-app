d88a61860cbb6d69b750ba37c14fd871b347330b
Marianna Mazzette <20104120@mail.wit.ie>
2026-03-24 18:23:53 +0000

    add hashed psw and downgrade bcrypt

--- backend/app/api/user_routes.py
+++ backend/app/api/user_routes.py
@@ -4,12 +4,15 @@ from sqlalchemy.orm import Session
 from app.api.dependencies import get_db
 from app.models.user import User
 from app.schemas import schemas
-
+from app.core import security
 
 router = APIRouter()
 
 @router.post("/users/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserResponse)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    
+    hashed_password = security.hash_password(user.password)
+    user.password = hashed_password
     new_user = User(**user.model_dump())
     db.add(new_user)
     db.commit()
