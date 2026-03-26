9fbbb13ba74cf08ed20dd1e001f8cc3f8ffd9acb
Marianna Mazzette <20104120@mail.wit.ie>
2026-03-26 16:36:29 +0000

    set up config db used for testing

---
+++ backend/app/tests/test_db_config.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.db.database import Base
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+TEST_DATABASE_URL = (
+    f"postgresql://{os.getenv('POSTGRES_USER')}:"
+    f"{os.getenv('POSTGRES_PASSWORD')}@"
+    f"{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/"
+    f"{os.getenv('TEST_POSTGRES_DB')}"
+)
+
+engine = create_engine(TEST_DATABASE_URL)
+TestingSessionLocal = sessionmaker(bind=engine)
+
+Base.metadata.create_all(bind=engine)
+
+
