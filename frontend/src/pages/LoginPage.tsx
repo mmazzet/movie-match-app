011eaa9f19e82fca8381cdf12c517b526b772035
Marianna Mazzette <20104120@mail.wit.ie>
2026-03-30 16:11:54 +0100

    add LoginPage

---
+++ frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+import { login } from '../services/authService'
+
+function LoginPage() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  const handleSubmit = async (e: React.SubmitEvent) => {
+    e.preventDefault()
+    setError('')
+    setLoading(true)
+    console.log('Form submitted!')
+    console.log('email:', email)
+    console.log('password:', password)
+
+    try {
+      const response = await login({ email, password })
+      console.log('Login successful!', response)
+    } catch {
+      setError('Invalid email or password.')
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return (
+    <div>
+      <h1>Login</h1>
+
+      {error && <p>{error}</p>}
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email</label>
+          <input
+            type="email"
+            placeholder="email@example.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+
+        <div>
+          <label>Password</label>
+          <input
+            type="password"
+            placeholder="••••••••"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+
+        <button type="submit" disabled={loading}>
+          {loading ? 'Logging in...' : 'Login'}
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default LoginPage
