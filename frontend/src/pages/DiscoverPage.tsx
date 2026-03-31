7b061503c9c3e85e2854c67b990c3c254cd4d56f
Marianna Mazzette <20104120@mail.wit.ie>
2026-03-31 23:19:25 +0100

    rename dashboard into discover

---
+++ frontend/src/pages/DiscoverPage.tsx
@@ -0,0 +1,10 @@
+function DiscoverPage() {
+  return (
+    <div>
+      <h1>Welcome to your Discover Page!</h1>
+      <p>This is where you can see the movies you like!</p>
+    </div>
+  )
+}
+
+export default DiscoverPage
