d356503d557a0b292f725dcee8c4cdcc501db3cf
Marianna Mazzette <20104120@mail.wit.ie>
2026-04-03 19:35:40 +0100

    add temporary landing page

---
+++ frontend/src/pages/LandingPage.tsx
@@ -0,0 +1,14 @@
+
+function LandingPage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-6">Welcome to Movie Match</h1>
+
+      <div>
+        <p>Log in or register to get started</p>
+      </div>
+    </div>
+  )
+}
+
+export default LandingPage
