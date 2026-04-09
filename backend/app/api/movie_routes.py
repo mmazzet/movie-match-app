3e7e6afaf39221c01727282ed446dd9b03ea4e7e
Marianna Mazzette <20104120@mail.wit.ie>
2026-04-09 13:36:23 +0100

    make popular protected route

--- backend/app/api/movie_routes.py
+++ backend/app/api/movie_routes.py
@@ -1,10 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from app.services.tmdb_service import get_popular_movies
 from app.schemas.movie_schema import MovieResponse
+from app.core.oauth2 import get_current_user
 
 router = APIRouter(tags=["movies"])
 
 @router.get("/movies/popular", response_model=list[MovieResponse])
-async def popular_movies():
+async def popular_movies(current_user = Depends(get_current_user)):
     data = await get_popular_movies()
     return data
