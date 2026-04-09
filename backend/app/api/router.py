32312bebe94ccf12ec9258d723e5876f58e1b0bc
Marianna Mazzette <20104120@mail.wit.ie>
2026-04-09 13:35:42 +0100

    update file name to auth_routes for consistency

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from app.api.health_routes import router as health_router
 from app.api.user_routes import router as user_router
-from app.api.auth_router import router as auth_router
+from app.api.auth_routes import router as auth_router
 from app.api.movie_routes import router as movie_router
 from app.api.likes_routes import router as likes_router
 
