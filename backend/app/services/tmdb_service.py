b5c4e981dad84c8b33bd5e3ced4b2034cec3a83e
Marianna Mazzette <20104120@mail.wit.ie>
2026-04-19 17:08:10 +0100

    check black automation

--- backend/app/services/tmdb_service.py
+++ backend/app/services/tmdb_service.py
@@ -9,20 +9,23 @@ load_dotenv()
 TMDB_BASE_URL = os.getenv("TMDB_BASE_URL")
 TMDB_API_KEY = os.getenv("TMDB_API_KEY")
 
+
 async def get_popular_movies():
     url = f"{TMDB_BASE_URL}/movie/popular"
     params = {"api_key": TMDB_API_KEY, "language": "en-IE", "region": "IE"}
-    
+
     try:
         async with httpx.AsyncClient() as client:
             response = await client.get(url, params=params)
 
             if response.status_code != 200:
-                logger.error("❌ TMDB API error: %s - %s", response.status_code, response.text)
+                logger.error(
+                    "❌ TMDB API error: %s - %s", response.status_code, response.text
+                )
                 raise ExternalServiceError("Failed to fetch movies from TMDB")
-            
+
             data = response.json()
-            
+
             movies = []
             for movie in data["results"]:
                 movie_info = {
@@ -33,7 +36,7 @@ async def get_popular_movies():
                     "overview": movie.get("overview"),
                 }
                 movies.append(movie_info)
-            
+
         logger.info("✅ Fetched %s popular movies from TMDB", len(movies))
 
         return movies
