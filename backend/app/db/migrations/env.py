e90f1671562b1659a5ba3bc62f77398ccc62a2df
Marianna Mazzette <20104120@mail.wit.ie>
2026-05-12 17:01:09 +0100

    add watched table model and migration

--- backend/app/db/migrations/env.py
+++ backend/app/db/migrations/env.py
@@ -11,6 +11,7 @@ from app.models.movie import Movie
 from app.models.like import Like
 from app.models.room import Room
 from app.models.room_member import RoomMember
+from app.models.watched import Watched
 
 import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../")))
